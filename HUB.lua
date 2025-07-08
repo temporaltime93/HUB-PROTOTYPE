fb9c02f4bfb0ccaf196a46de241c02c262e3a711
temporaltime93 <temporaltime93@gmail.com>
2025-07-07 23:10:53 -0500

    Create HUB.lua

---
+++ HUB.lua
@@ -0,0 +1,27 @@
+if MADE_BY ~= "TEAM_PROTOTYPE" or DISCORD ~= "https://discord.gg/2qcRceCmtC" then
+    error("Este script no funcionará sin los créditos originales.")
+end
+
+
+if DESACTIVAR_BLOQUEO == "SI" then
+    spawn(function()
+        while true do
+            if game.UserInputService.MouseBehavior ~= Enum.MouseBehavior.Default then
+                game.UserInputService.MouseBehavior = Enum.MouseBehavior.Default
+            end
+            wait(0.5)
+        end
+    end)
+else
+    print("no")
+end
+
+
+if _G.CARGA == "VIRUS" then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/temporaltime93/-HUBS-/main/PARTES/parte2.lua"))()
+else
+    _G.CARGA = "VIRUS" 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/temporaltime93/-PROTOTYPE-/main/[CARGAS]/carga.lua"))()
+    wait(2)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/temporaltime93/-PROTOTYPE-/main/[MODULOS]/NOTI.lua"))()
+end
