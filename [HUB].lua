684cad410ba1db4eff1b134e02217f3a429fe4b6
temporaltime93 <temporaltime93@gmail.com>
2025-07-07 23:37:17 -0500

    Update [HUB].lua

--- [HUB].lua
+++ [HUB].lua
@@ -18,10 +18,10 @@ end
 
 
 if _G.CARGA == "VIRUS" then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/temporaltime93/-HUBS-/main/PARTES/parte2.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/temporaltime93/HUB-PROTOTYPE/refs/heads/main/CONNECTORS/%5BSCRTIPS%5D.lua"))()
 else
     _G.CARGA = "VIRUS" 
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/temporaltime93/-PROTOTYPE-/main/[CARGAS]/carga.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/temporaltime93/HUB-PROTOTYPE/refs/heads/main/CONNECTORS/%5BCARGA%5D.lua"))()
     wait(2)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/temporaltime93/HUB-PROTOTYPE/refs/heads/main/UTILITIES/%5BNOTI%5D.lua"))()
 end
